d9bec44e062984590c8928c140a18e4ae5861555
artemserba <artem.serba@mail.com>
2017-03-15 20:58:58 +0200

    Init gitignore, pom, main boot.

---
+++ JCPLProject/src/main/java/com/jcplproject/boot/Main.java
@@ -0,0 +1,12 @@
+package com.jcplproject.boot;
+
+/**
+ *
+ * @author Artem S
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        System.out.println("Welcome!");
+    }
+}
